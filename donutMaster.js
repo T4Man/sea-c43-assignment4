c29cf51b59f5176daf07a5c0b539ecdbae84d4e3
t4man <timothywforman@gmail.com>
2015-08-15 17:59:38 -0700

    updated html and js, added css

--- donutMaster.js
+++ donutMaster.js
@@ -1,3 +1,5 @@
+//javascript for donutMaster.html CodeFellows Foundations 1 Assignment 4
+
 var DonutMaster = function(location, minCustPerHour, maxCustPerHour,
     avgDonutPerCust, businessHours) {
   this.location = location;
@@ -5,26 +7,21 @@ var DonutMaster = function(location, minCustPerHour, maxCustPerHour,
   this.max = maxCustPerHour;
   this.avg = avgDonutPerCust;
   this.hrs = businessHours;
-
   this.donutsPerHour = function() {
   return Math.round(Math.random() * (this.max - this.min + 1) +
     this.min) * this.avg;
   };
-
   this.donutsPerDay = function() {
   return Math.round(this.donutsPerHour() * this.hrs);
   };
 };
-
 var stores = [];
-
 var addNewShop = function(location, minCustPerHour, maxCustPerHour,
   avgDonutPerCust, businessHours) {
   var newShop = new DonutMaster(location, minCustPerHour, maxCustPerHour,
     avgDonutPerCust, businessHours);
   stores.push(newShop);
 };
-
 var generateReport = function() {
   for (var index = 0; index < stores.length; index++) {
     console.log("The " + stores[index].location + " location averages " +
@@ -32,11 +29,12 @@ var generateReport = function() {
     Math.round(this.stores[index].donutsPerDay()) + " donuts per day.");
   }
 };
-  addNewShop("new", 4, 49, 4, 16);
-  addNewShop("Downtown", 8, 43, 4.5, 16);
-  addNewShop("Capitol Hill", 4, 37, 2.00, 12);
-  addNewShop("South Lake Union", 9, 12, 6.33, 12);
-  addNewShop("Wedgewood",2, 28, 1.25, 10);
-  addNewShop("Ballard", 8, 58, 3.75, 14);
 
-  generateReport();
+addNewShop("new", 4, 49, 4, 10);
+addNewShop("Downtown", 8, 43, 4.5, 16);
+addNewShop("Capitol Hill", 4, 37, 2.00, 12);
+addNewShop("South Lake Union", 9, 12, 6.33, 12);
+addNewShop("Wedgewood",2, 28, 1.25, 10);
+addNewShop("Ballard", 8, 58, 3.75, 14);
+
+generateReport();
